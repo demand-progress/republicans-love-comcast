313c366ba1a06363da657701ba88beabfb94970c
Josh Levinger <josh@levinger.net>
2014-12-04 14:45:40 -0800

    first commit, forked from call-google

---
+++ _includes/share_tweet_text.js
@@ -0,0 +1,6 @@
+//
+//  WARNING! ~ If you need to use an apostrophe ==>'<==, precede it with a backslash
+//  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
+//  If you fail to do this, everything will be ruined.
+//
+TWEET_TEXT = 'Tell @googlepubpolicy @google that it\'s time for them to act like they really support #NetNeutrality. http://callongoogle.net #CallOnGoogle'
