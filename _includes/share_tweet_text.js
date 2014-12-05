36e4e6ec152c68a14c577a96007e201ecb8b73a5
Josh Levinger <josh@levinger.net>
2014-12-05 10:36:57 -0800

    republicans love comcast content, colors

--- _includes/share_tweet_text.js
+++ _includes/share_tweet_text.js
@@ -3,4 +3,4 @@
 //  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
 //  If you fail to do this, everything will be ruined.
 //
-TWEET_TEXT = 'Tell @googlepubpolicy @google that it\'s time for them to act like they really support #NetNeutrality. http://callongoogle.net #CallOnGoogle'
+TWEET_TEXT = 'Who loves @Comcast? Tell these @GOP lawmakers to protect #netneutrality + stop shilling for Big Cable. http://RepublicansLoveComcast.com';
